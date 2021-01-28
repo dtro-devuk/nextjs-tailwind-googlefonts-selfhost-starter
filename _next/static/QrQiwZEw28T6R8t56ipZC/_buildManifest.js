5df0d5d87675cff132d1153f656ec48cadaab19a
NET Stack & JS Frameworks <dtro-devuk@users.noreply.github.com>
2021-01-28 19:00:36 +0000

    Deploy gh-pages

--- _next/static/QrQiwZEw28T6R8t56ipZC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-734fb407e94111fc4083.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e2ffa4674eea62b50f3f.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
